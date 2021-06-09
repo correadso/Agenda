279fd98e84c50b8f8e7d553032e0df339b5f565d
Diogo <diogocorrea@tecnomotor.com.br>
2021-06-09 17:49:57 -0300

    Implementando comportamentos do DAO

--- app/src/main/java/br/com/alura/agenda/AgendaApplication.java
+++ app/src/main/java/br/com/alura/agenda/AgendaApplication.java
@@ -2,7 +2,11 @@ package br.com.alura.agenda;
 
 import android.app.Application;
 
+import androidx.room.Room;
+
 import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.database.AgendaDatabase;
+import br.com.alura.agenda.database.dao.RoomAlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
 @SuppressWarnings("unused")
@@ -15,7 +19,10 @@ public class AgendaApplication extends Application {
     }
 
     private void criaAlunosDeTeste() {
-        AlunoDAO dao = new AlunoDAO();
+        //AlunoDAO dao = new AlunoDAO();
+        AgendaDatabase database = Room.databaseBuilder(this, AgendaDatabase.class, "agenda.db").build();
+        // para funcionar essa linha tem que implementar o método que retorna o DAO na AgendaDatabase
+        RoomAlunoDAO dao = database.getRoomAlunoDAO();
         dao.salva(new Aluno("Diogo", "112233", "diogo@tecno.com.br"));
         dao.salva(new Aluno("Ândrea", "998800", "andrea@gmail.com"));
     }
--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -3,8 +3,11 @@ package br.com.alura.agenda.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import br.com.alura.agenda.database.dao.RoomAlunoDAO;
 import br.com.alura.agenda.model.Aluno;
 
 @Database(entities = {Aluno.class}, version = 1, exportSchema = false)
 public abstract class AgendaDatabase extends RoomDatabase {
+    public abstract RoomAlunoDAO getRoomAlunoDAO(); // a classe RoomAlunoDAO deve ter a anotação DAO
 }
+
