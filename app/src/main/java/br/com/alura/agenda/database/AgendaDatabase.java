5acf9fd679216f52320128f0f2ef699313877b08
Diogo <diogocorrea@tecnomotor.com.br>
2021-06-09 19:51:00 -0300

    Utilizando singleton e synchronized

--- app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
+++ app/src/main/java/br/com/alura/agenda/database/AgendaDatabase.java
@@ -13,11 +13,17 @@ import br.com.alura.agenda.model.Aluno;
 public abstract class AgendaDatabase extends RoomDatabase {
     public abstract AlunoDAO getRoomAlunoDAO(); // a classe AlunoDAO deve ter a anotação DAO
     private static final String NOME_BANCO_DE_DADOS = "agenda.db";
+    private static AgendaDatabase instance; // utilizando singleton
 
-    public static AgendaDatabase getInstance(Context context) {
-        return Room.databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
-                .allowMainThreadQueries() // não recomendado, apenas para testar
-                .build();
+    // synchronized static permite que apenas uma Thread execute o método por vez,
+    // assim não gera instâncias diferentes do database se duas Threads tentarem acessar ao mesmo tempo o método
+    public synchronized static AgendaDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, AgendaDatabase.class, NOME_BANCO_DE_DADOS)
+                    .allowMainThreadQueries() // não recomendado, apenas para testar
+                    .build();
+        }
+        return instance;
     }
 }
 
